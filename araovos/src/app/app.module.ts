53f04edbcdb7beee7c1333a3314b5cb2a5b6c000
Gabriel Lopes <carnagesagas@gmail.com>
2024-03-12 17:17:36 -0300

    tentativa de colocar uma mensagem para erros de validação
    não esta aparecendo a mensagem

--- araovos/src/app/app.module.ts
+++ araovos/src/app/app.module.ts
@@ -5,18 +5,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TecnicosListComponent } from './components/tecnico/tecnicos-list/tecnicos-list.component';
+
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
-import { NavComponent } from './components/nav/nav.component';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { MenuModule } from 'primeng/menu';
-import { HomeComponent } from './components/home/home.component';
 import { CardModule } from 'primeng/card';
-import { HeaderComponent } from './components/header/header.component';
-import { TecnicosListComponent } from './components/tecnico/tecnicos-list/tecnicos-list.component';
 import { TableModule } from 'primeng/table';
-import { LoginComponent } from './components/login/login.component';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [
@@ -39,10 +43,13 @@ import { InputTextModule } from 'primeng/inputtext';
     SidebarModule,
     TabMenuModule,
     TableModule,
-    InputTextModule
+    InputTextModule,
+    ToastModule,
+    MessageModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
--- araovos/src/app/components/login/login.component.ts
+++ araovos/src/app/components/login/login.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Credenciais } from '../modelos/credenciais';
 import { FormControl, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(private messageService: MessageService) {}
+  
   username: string;
   password: string;
   passwordVisible: boolean = false;
@@ -20,14 +25,25 @@ export class LoginComponent {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  togglePasswordVisibility() {
-    this.passwordVisible = !this.passwordVisible;
+  
+
+  ngOnInit(): void {
+
+  }
+
+  logar() {
+    console.log('Tentativa de login');
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Message Content', life: 3000 });
+    console.log('depois da mensagem');
   }
 
   validarCampos(): boolean {
+    console.log('Tentativa de validar');
     if(this.email.valid && this.senha.valid) {
+      console.log('deu certo');     
       return true;
     } else {
+      console.log('deu errado');
       return false;
     }
   }
