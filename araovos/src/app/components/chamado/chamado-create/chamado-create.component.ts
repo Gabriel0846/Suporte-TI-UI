57c972e0a330921d5c00657add8b5a9ad32cdd1c
Gabriel Lopes <carnagesagas@gmail.com>
2024-03-18 17:22:10 -0300

    post para criar chamados (precisa de ajustes)

--- araovos/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ araovos/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -3,15 +3,20 @@ import { Chamado } from '../../../modelos/chamado';
 import { FormControl, Validators } from '@angular/forms';
 import { Cliente } from '../../../modelos/cliente';
 import { Tecnico } from '../../../modelos/tecnico';
+import { ChamadoService } from '../../../services/chamado.service';
+import { ClienteService } from '../../../services/cliente.service';
+import { TecnicoService } from '../../../services/tecnico.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 interface Status {
   name: string;
-  value: string;
+  value: number;
 }
 
 interface Prioridade {
   name: string;
-  value: string;
+  value: number;
 }
 
 @Component({
@@ -21,27 +26,26 @@ interface Prioridade {
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  selectedPrioridade: Prioridade | undefined;
+  selectedStatus: Status | undefined; 
+
   chamado: Chamado = {
-    prioridade:  null,
-    status:      null,
-    titulo:      '',
+    prioridade: null,
+    status: null,
+    titulo: '',
     observacoes: '',
-    tecnico:     '',
-    cliente:     '',
+    tecnico: '',
+    cliente: '',
     nomeCliente: '',
     nomeTecnico: '',
   }
 
-  clientes: Cliente[] = []
-  tecnicos: Tecnico[] = []
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
 
   status: Status[] = [];
   prioridade: Prioridade[] = [];
 
-  selectedStatus: Status | undefined;
-  selectedPrioridade: Prioridade | undefined;
-
-  // Adicione as propriedades ao componente
   prioridadeValid: boolean = false;
   statusValid: boolean = false;
   tituloValid: boolean = false;
@@ -56,30 +60,62 @@ export class ChamadoCreateComponent implements OnInit {
   tecnicoControl: FormControl = new FormControl(null, [Validators.required]);
   clienteControl: FormControl = new FormControl(null, [Validators.required]);
 
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toastService: ToastrService,
+    private router: Router,
+  ) {}
+
   ngOnInit() {
     this.status = [
-      { name: 'ABERTO', value: '0' },
-      { name: 'EM ANDAMENTO', value: '1' },
-      { name: 'ENCERRADO', value: '3' }
+      { name: 'ABERTO', value: 0 },
+      { name: 'EM ANDAMENTO', value: 1 },
+      { name: 'ENCERRADO', value: 3 }
     ];
 
     this.prioridade = [
-      { name: 'BAIXA', value: '0' },
-      { name: 'MEDIA', value: '1' },
-      { name: 'ALTA', value: '3' }
+      { name: 'BAIXA', value: 0 },
+      { name: 'MEDIA', value: 1 },
+      { name: 'ALTA', value: 3 }
     ];
+
+    this.findAllClientes();
+    this.findAllTecnicos();
   }
 
-  validaCampos(): boolean {
-    // Verifica a validade dos campos individualmente
-    this.prioridadeValid = this.prioridadeControl.valid;
-    this.statusValid = this.statusControl.valid;
-    this.tituloValid = this.tituloControl.valid;
-    this.observacoesValid = this.observacoesControl.valid;
-    this.tecnicoValid = this.tecnicoControl.valid;
-    this.clienteValid = this.clienteControl.valid;
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(resposta => {
+      this.toastService.success('Chamado criado com sucesso', 'Novo chamado');
+      this.router.navigate(['chamados']);
+    }, ex => {
+      console.log(ex);
+      
+      this.toastService.error(ex.error.error);
+    })
+  }
+  
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta as Cliente[];
+    })
+  }
 
-    // Retorna true apenas se todos os campos forem válidos
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta as Tecnico[];
+    });
+  }
+  
+  validaCampos(): boolean {
+    this.prioridadeValid = this.prioridadeControl.valid && this.chamado.prioridade !== null;
+    this.statusValid = this.statusControl.valid && this.chamado.status !== null;
+    this.tituloValid = this.tituloControl.valid && this.chamado.titulo.trim() !== '';
+    this.observacoesValid = this.observacoesControl.valid && this.chamado.observacoes.trim() !== '';
+    this.tecnicoValid = this.tecnicoControl.valid && this.chamado.tecnico !== null;
+    this.clienteValid = this.clienteControl.valid && this.chamado.cliente !== null;
+
     return this.prioridadeValid && this.statusValid && this.tituloValid 
        && this.observacoesValid && this.tecnicoValid && this.clienteValid;
   }
