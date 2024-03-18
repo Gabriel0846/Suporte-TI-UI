eb85e6b4f8ffe5cb7d3b906bdc87c0b142121765
Gabriel Lopes <carnagesagas@gmail.com>
2024-03-18 14:22:20 -0300

    validacao de formularios criar chamados

--- araovos/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ araovos/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chamado } from '../../../modelos/chamado';
+import { FormControl, Validators } from '@angular/forms';
+import { Cliente } from '../../../modelos/cliente';
+import { Tecnico } from '../../../modelos/tecnico';
 
 interface Status {
   name: string;
@@ -14,25 +17,70 @@ interface Prioridade {
 @Component({
   selector: 'app-chamado-create',
   templateUrl: './chamado-create.component.html',
-  styleUrl: './chamado-create.component.css'
+  styleUrls: ['./chamado-create.component.css']
 })
-export class ChamadoCreateComponent implements OnInit{
-  status: Status[] | undefined;
-  prioridade: Prioridade[] | undefined;
+export class ChamadoCreateComponent implements OnInit {
 
-    selectedStatus: Status | undefined;
-    selectedPrioridade: Prioridade | undefined;
+  chamado: Chamado = {
+    prioridade:  null,
+    status:      null,
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
 
-    ngOnInit() {
-        this.status = [
-            { name: 'ABERTO', value: '0' },
-            { name: 'EM ANDAMENTO', value: '1' },
-            { name: 'ENCERRADO', value: '3' }
-        ];
-        this.prioridade = [
-          { name: 'BAIXA', value: '0' },
-          { name: 'MEDIA', value: '1' },
-          { name: 'ALTA', value: '3' }
-      ];
-    }
+  clientes: Cliente[] = []
+  tecnicos: Tecnico[] = []
+
+  status: Status[] = [];
+  prioridade: Prioridade[] = [];
+
+  selectedStatus: Status | undefined;
+  selectedPrioridade: Prioridade | undefined;
+
+  // Adicione as propriedades ao componente
+  prioridadeValid: boolean = false;
+  statusValid: boolean = false;
+  tituloValid: boolean = false;
+  observacoesValid: boolean = false;
+  tecnicoValid: boolean = false;
+  clienteValid: boolean = false;
+
+  prioridadeControl: FormControl = new FormControl(null, [Validators.required]);
+  statusControl: FormControl = new FormControl(null, [Validators.required]);
+  tituloControl: FormControl = new FormControl(null, [Validators.required]);
+  observacoesControl: FormControl = new FormControl(null, [Validators.required]);
+  tecnicoControl: FormControl = new FormControl(null, [Validators.required]);
+  clienteControl: FormControl = new FormControl(null, [Validators.required]);
+
+  ngOnInit() {
+    this.status = [
+      { name: 'ABERTO', value: '0' },
+      { name: 'EM ANDAMENTO', value: '1' },
+      { name: 'ENCERRADO', value: '3' }
+    ];
+
+    this.prioridade = [
+      { name: 'BAIXA', value: '0' },
+      { name: 'MEDIA', value: '1' },
+      { name: 'ALTA', value: '3' }
+    ];
+  }
+
+  validaCampos(): boolean {
+    // Verifica a validade dos campos individualmente
+    this.prioridadeValid = this.prioridadeControl.valid;
+    this.statusValid = this.statusControl.valid;
+    this.tituloValid = this.tituloControl.valid;
+    this.observacoesValid = this.observacoesControl.valid;
+    this.tecnicoValid = this.tecnicoControl.valid;
+    this.clienteValid = this.clienteControl.valid;
+
+    // Retorna true apenas se todos os campos forem válidos
+    return this.prioridadeValid && this.statusValid && this.tituloValid 
+       && this.observacoesValid && this.tecnicoValid && this.clienteValid;
+  }
 }
